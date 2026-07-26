4ef72be557c97742846522ee520569049181edd3
drheavy <drheavy@yandex.ru>
2026-07-26 21:22:00 +0300

    Update contact phone number

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,6 +1,6 @@
 /* Временные контакты меняются только здесь. */
 window.PK_CONFIG = Object.freeze({
-  phoneDisplay: "+7 915 567-71-71",
-  phoneHref: "+79155677171",
+  phoneDisplay: "+7 916 567-71-71",
+  phoneHref: "+79165677171",
   email: "info@promkinematika.ru"
 });
